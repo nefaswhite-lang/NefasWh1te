35554d394f2bb4b17571c9a349d7cfbf3d1b299b
nefaswhite-lang <nefaswhite@gmail.com>
2025-08-19 18:40:03 +0200

    Replace with Next.js styled site
    Replace with Next.js styled site

--- next.config.js
+++ next.config.js
@@ -1,3 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { experimental: { appDir: true } }
-module.exports = nextConfig
+const nextConfig = { experimental: { appDir: true } };
+module.exports = nextConfig;
